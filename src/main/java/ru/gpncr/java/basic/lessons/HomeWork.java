22a5add55b76f161188629c422b4eca7b8cf936b
Denis Yakovlev <clerik1987@mail.ru>
2024-07-12 19:49:04 +0300

    Lesson 2

--- src/main/java/ru/gpncr/java/basic/lessons/HomeWork.java
+++ src/main/java/ru/gpncr/java/basic/lessons/HomeWork.java
@@ -1,7 +1,19 @@
 package ru.gpncr.java.basic.lessons;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class HomeWork {
     public static void main(String[] args) {
         System.out.println("HomeWork");
+
+        printString(5, "HomeWork: Lesson 2");
+    }
+
+    public static void printString(int n, String s) {
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + 1 + ". " + s);
+        }
     }
 }
