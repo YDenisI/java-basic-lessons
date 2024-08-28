f30002c4bf19d219f0a4b31f4ac47cdfa43c19c7
Denis Yakovlev <clerik1987@mail.ru>
2024-08-28 20:40:33 +0300

    Lesson Algoritm

--- src/main/java/ru/gpncr/java/basic/lessons/HomeWork.java
+++ src/main/java/ru/gpncr/java/basic/lessons/HomeWork.java
@@ -2,6 +2,16 @@ package ru.gpncr.java.basic.lessons;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("HomeWork");
+        PersonDataBase dataBase = new PersonDataBase();
+
+        Person person1 = new Person("Denis", Position.MANAGER, 1L);
+        Person person2 = new Person("John", Position.DEVELOPER, 2L);
+
+        dataBase.add(person1);
+        dataBase.add(person2);
+
+        System.out.println(dataBase.findById(1L).getName());
+        System.out.println(dataBase.isManager(person1));
+        System.out.println(dataBase.isEmployee(2L));
     }
 }
