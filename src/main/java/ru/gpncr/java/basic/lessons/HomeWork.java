721ea85babdc7734686dcd578551c59735ab3ae6
Denis Yakovlev <clerik1987@mail.ru>
2024-10-08 12:22:30 +0300

    MultiThread

--- src/main/java/ru/gpncr/java/basic/lessons/HomeWork.java
+++ src/main/java/ru/gpncr/java/basic/lessons/HomeWork.java
@@ -1,7 +1,76 @@
 package ru.gpncr.java.basic.lessons;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HomeWork {
+    private static final Object lock = new Object();
+    private static char currentChar = 'A';
+    private static final int COUNT = 5;
+
     public static void main(String[] args) {
 
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Thread printA = new Thread(() -> {
+            for (int i = 0; i < COUNT; i++) {
+                synchronized (lock) {
+                    while (currentChar != 'A') {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    System.out.print("A");
+                    currentChar = 'B';
+                    lock.notifyAll();
+                }
+            }
+        });
+
+
+        Thread printB = new Thread(() -> {
+            for (int i = 0; i < COUNT; i++) {
+                synchronized (lock) {
+                    while (currentChar != 'B') {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    System.out.print("B");
+                    currentChar = 'C';
+                    lock.notifyAll();
+                }
+            }
+        });
+
+
+        Thread printC = new Thread(() -> {
+            for (int i = 0; i < COUNT; i++) {
+                synchronized (lock) {
+                    while (currentChar != 'C') {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    System.out.print("C");
+                    currentChar = 'A';
+                    lock.notifyAll();
+                }
+            }
+        });
+
+
+        executor.execute(printA);
+        executor.execute(printB);
+        executor.execute(printC);
+
+        executor.shutdown();
     }
+
 }
